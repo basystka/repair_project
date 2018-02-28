84af3af6d9b89e6dce5d8ad3cb58e3091defcbd2
basystka <basystka@gmail.com>
2018-02-28 13:11:22 +0200

    first commit

---
+++ js/slide.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		setting-name: setting-value
+	});
+});
+$('.slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
